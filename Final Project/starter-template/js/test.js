e4d37a65b8faf4de77d242d84e2081d571646d57
campb822 <sacampbell2014@gmail.com>
2016-12-09 16:17:04 -0500

    finishing touches

--- Final Project/starter-template/js/test.js
+++ Final Project/starter-template/js/test.js
@@ -1,16 +1,3 @@
-/*$(function(){  // $(document).ready shorthand
-  $("#helloworld").hide().fadeIn('slow');
-});
-
-
-  $("button").on("click", function() {
-    alert("Clicked!");
-  });
-
-function myFunction() {
-  document.getElementById("demo").innerHTML = "hello world";
-}
-*/
 
 $(document).ready(function() {
   $("#helloworld").delay(500).fadeIn(2000);
@@ -22,21 +9,59 @@ $(document).ready(function() {
   $("#fade_in").delay(1500).fadeIn(2000);
 });
 $(document).ready(function() {
-  $("#fade_in2").delay(2000).slideDown(1000);
-});
-
-$(document).ready(function() {
-  $("#homepage").fadeIn("normal");
+  $("#fade_in6").delay(1500).fadeIn(2000);
 });
 $(document).ready(function() {
-  $("#contactme").fadeIn("normal");
+  $("#fade_in2").delay(2000).fadeIn(2000);
 });
 $(document).ready(function() {
-  $("#proj").fadeIn("normal");
+  $("#fade_in3").delay(2500).fadeIn(1000);
 });
 $(document).ready(function() {
-  $("#edu").fadeIn("normal");
+  $("#fade_in4").delay(3000).fadeIn(1000);
 });
 $(document).ready(function() {
-  $("#extra").fadeIn("normal");
+  $("#fade_in5").delay(2000).fadeIn(1000);
+});
+$(document).ready(function() {
+  $("#fade_in7").delay(5000).fadeIn(1000);
+});
+$(document).ready(function() {
+  $("#homepage").fadeOut(500).fadeIn(500);
+});
+$(document).ready(function() {
+  $("#contactme").delay(500).fadeIn(2000);
+});
+$(document).ready(function() {
+  $("#proj").delay(500).fadeIn(2000);
+});
+$(document).ready(function() {
+  $("#edu").delay(500).fadeIn(2000);
+});
+$(document).ready(function() {
+  $("#extra").delay(500).fadeIn(2000);
+});
+$(document).ready(function() {
+  $("#trumpet").delay(500).fadeIn("slow");
+});
+$(document).ready(function() {
+  $("#trumpet2").delay(1500).fadeIn("slow");
+});
+$(document).ready(function() {
+  $("#extra_p").delay(2000).fadeIn("slow");
+});
+$(document).ready(function() {
+  $("#proj2").delay(2000).fadeIn("fast");
+});
+$(document).ready(function() {
+  $("#proj3").delay(2000).fadeIn("normal");
+});
+$(document).ready(function() {
+  $("#proj4").delay(4000).fadeIn("normal");
+});
+$(document).ready(function() {
+  $("#contact").delay(2000).fadeIn("fast");
+});
+$(document).ready(function() {
+  $("#morecontact").delay(2000).fadeIn("fast");
 });
